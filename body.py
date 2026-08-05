3f26a91095a986def61011a60ee41a075fcaea44
rhydian98 <rhydian98@gmail.com  git config --global user.name  rhydian  git config --global user.email rhydian98@gmail.com>
2026-08-05 10:35:33 +0100

    Added a separate file for Body class

---
+++ body.py
@@ -0,0 +1,20 @@
+class Body:
+    def __init__(self, name, mass, color, x=100 ,y=100, radius=1, vx=0, vy=0, ax=0, ay=0):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.color = color
+        self.radius = radius
+        self.vx = vx
+        self.vy = vy
+        self.ax = ax
+        self.ay = ay
+
+
+    def update_position(self, dt):
+        self.vx += self.ax * dt
+        self.vy += self.ay * dt
+
+        self.x += self.vx * dt
+        self.y += self.vy * dt
