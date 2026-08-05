2d4ddbf4c5fd517bebe3037aaf48a3e98b8a8661
rhydian98 <rhydian98@gmail.com  git config --global user.name  rhydian  git config --global user.email rhydian98@gmail.com>
2026-08-05 10:34:22 +0100

    Added simulation class to handle updating the bodies, and moved the bodies class out of the main file into its own file

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import pygame
 from pygame.time import Clock
-class Planet:
-    def __init__(self, name, mass, color, x=100 ,y=100, radius=1):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.mass = mass
-        self.color = color
-        self.radius = radius
-        self.vx = 0
-        self.vy = 0
+from simulation import Simulation
+
+from body import Body
 
 
-planets = [
-    Planet("Sun", 1.989e30, (255, 255, 0), 400, 300, 10),
-    Planet("Earth", 5.972e24, (0, 0, 255), 500, 200, 2),
-    Planet("Mars", 6.417e23, (255, 0, 0), 600, 400, 2),
+
+bodies = [
+    Body("Sun", 1.989e30, (255, 255, 0), 400, 300, 10, vx=100, vy=0),
+    Body("Earth", 5.972e24, (0, 0, 255), 500, 200, 2, vx=0, vy=0),
+    Body("Mars", 6.417e23, (255, 0, 0), 600, 400, 2),
 
 ]
 
@@ -25,18 +19,17 @@ def main():
     pygame.display.set_caption("Orbital Simulation")
     running = True
     clock = Clock()
-
+    simulation = Simulation(bodies, screen)
     while running:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        for planet in planets:
-            pygame.draw.circle(screen, planet.color, (planet.x, planet.y), planet.radius*2)
-            planet.x += planet.vx
-            planet.y += planet.vy
-        pygame.display.flip()
-        clock.tick(60)
+        simulation.update(dt)
+
+        simulation.draw()
+
     pygame.quit()
 
 if __name__ == "__main__":
--- simulation.py
+++ simulation.py
@@ -1,2 +1,20 @@
 import pygame
-from pygame import Clock
+from pygame.time import Clock
+
+
+class Simulation:
+    def __init__(self, bodies, screen):
+        self.bodies = bodies
+        self.screen = screen
+        self.clock = Clock()
+
+    def update(self, dt):
+        for body in self.bodies:
+            body.update_position(dt)
+
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        for body in self.bodies:
+            pygame.draw.circle(self.screen, body.color, (body.x, body.y), body.radius)
+        pygame.display.flip()
