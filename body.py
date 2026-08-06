cae7381397bf5471e5f5047d8895d0501fcd8c39
rhydian98 <rhydian98@gmail.com>
2026-08-06 14:58:58 +0100

    Added a draw function per planet enabling trails

--- body.py
+++ body.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+import pygame
 class Body:
     def __init__(self, name, mass, color, x=0.0 ,y=0.0, radius=1, vx=0, vy=0, ax=0, ay=0):
         self.name = name
@@ -10,7 +13,7 @@ class Body:
         self.vy = vy
         self.ax = ax
         self.ay = ay
-        self.train = []
+        self.trail = deque(maxlen=200)
 
 
     def update_position(self, dt):
@@ -19,3 +22,19 @@ class Body:
 
         self.x += self.vx * dt
         self.y += self.vy * dt
+
+        self.trail.append((self.x, self.y))
+
+
+    def draw(self, screen, screen_position, trail_screen_pos, font):
+        print(self.name, len(self.trail), len(trail_screen_pos))
+
+
+        for trailpos in trail_screen_pos:
+            pygame.draw.circle(screen, self.color, trailpos, max(1,self.radius//2))
+
+        pygame.draw.circle(screen, self.color, (screen_position), self.radius)
+
+        label = font.render(self.name, True, self.color)
+
+        screen.blit(label, (screen_position[0] + self.radius + 5, screen_position[1] - self.radius))
