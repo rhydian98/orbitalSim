bd60b4ec5c07d49be8bd172bacb4e6cfe23e8c06
rhydian98 <rhydian98@gmail.com>
2026-08-09 19:42:30 +0100

    Added a telemetry menu
    Added a menu to allow the user to select telemetry which they want to be displayed on the per body info screen

--- body.py
+++ body.py
@@ -52,3 +52,24 @@ class Body:
         self.label_rect = label.get_rect(topleft=label_position)
 
         screen.blit(label, label_position)
+
+
+    def draw_info_box(self, screen, screen_position, font, lines):
+        speed = self.get_speed()/1000
+        distance = self.get_distance()/1e9
+
+
+
+        box_x = screen_position[0] + 20
+        box_y = screen_position[1] + 20
+
+        box_width=220
+        box_height=80
+
+        pygame.draw.rect(screen, (30,30,30), (box_x, box_y, box_width, box_height))
+
+        y=box_y +8
+        for line in lines:
+            text = font.render(line, True, (255,255,255))
+            screen.blit(text, (box_x + 8, y))
+            y +=22
--- simulation.py
+++ simulation.py
@@ -13,6 +13,17 @@ class Simulation:
         self.simulation_time = 0
         self.selected_body = None
 
+
+        self.telemetry_options = {
+            "speed": True,
+            "distance": True,
+            "mass": True,
+            "velocity": False,
+            "acceleration": False
+        }
+
+        self.telemetry_menu_rects = {}
+
     def update(self, dt):
         self.simulation_time += dt
         self.apply_gravity()
@@ -32,8 +43,62 @@ class Simulation:
         for body in self.bodies:
             if body.label_rect and body.label_rect.collidepoint(mouse_pos):
                 self.selected_body = body
-                print(f"{self.selected_body.name}, {self.selected_body.get_speed()/1000:.1f}Km/s,{self.selected_body.get_distance()/1e9: .1f} million Km ")
                 break
+
+        for key, rect in self.telemetry_menu_rects.items():
+            if rect.collidepoint(mouse_pos):
+                self.telemetry_options[key] = not self.telemetry_options[key]
+                return
+
+
+
+    def build_info_box(self, body):
+
+        lines = [body.name]
+
+        if self.telemetry_options["speed"]:
+            speed = body.get_speed() / 1000
+            lines.append(f"Speed: {speed:.1f} km/s")
+
+        if self.telemetry_options["distance"]:
+            distance = body.get_distance() / 1e9
+            lines.append(f"Distance: {distance:.1f} million km")
+
+        if self.telemetry_options["mass"]:
+            lines.append(f"Mass: {body.mass:.2e} kg")
+
+        if self.telemetry_options["velocity"]:
+            lines.append(f"Vx: {body.vx / 1000:.1f} km/s")
+            lines.append(f"Vy: {body.vy / 1000:.1f} km/s")
+
+        return lines
+
+
+    def draw_tel_menu(self,screen):
+        options = [
+            ("speed", "Speed"),
+            ("mass", "Mass"),
+            ("distance", "Distance"),
+            ("velocity", "Velocity")
+        ]
+
+        x = 20
+        y = 60
+
+        for key, label in options:
+            checkbox_rect = pygame.Rect(x,y, 18,18)
+
+            pygame.draw.rect(screen, (255,255,255), checkbox_rect,2)
+
+            if self.telemetry_options[key]:
+                pygame.draw.line(screen, (255,255,255), checkbox_rect.topleft, checkbox_rect.bottomright, 2)
+
+            text = self.font.render(label, True, (255,255,255))
+            screen.blit(text, (x+28, y - 2))
+
+            self.telemetry_menu_rects[key] = checkbox_rect
+
+            y += 28
 
 
 
@@ -51,9 +116,15 @@ class Simulation:
 
             body.draw(self.screen, body_screen_pos, trail_position, self.font)
 
+            lines = self.build_info_box(body)
+
+
+            if body ==  self.selected_body:
+                body.draw_info_box(self.screen, body_screen_pos, self.font, lines)
+
         simulation_days = self.simulation_time / 86400
         simulation_years = simulation_days / 365.25
-
+        self.draw_tel_menu(self.screen)
         clock_text = self.font.render(
             f"Simulation Time: {simulation_years:.2f} years",
             True,
